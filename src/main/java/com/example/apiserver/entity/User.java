5bf8ec541dbee11b8bbb916e4feff86c0bbc0217
taewoocode <qkrxodn6035@gmail.com>
2024-07-19 20:41:37 +0900

    유저클래스 구현

---
+++ src/main/java/com/example/apiserver/entity/User.java
@@ -0,0 +1,64 @@
+package com.example.apiserver.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+import java.util.Objects;
+
+@Getter
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals( id, user.id ) && Objects.equals( name, user.name ) && Objects.equals( email, user.email );
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash( id, name, email );
+    }
+}
